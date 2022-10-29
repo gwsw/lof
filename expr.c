02b1cab2cd1ef1d9ab779d5fddffa0fe30ab77a7
Mark Nudelman <markn@greenwoodsoftware.com>
2022-10-29 13:09:20 -0700

    Fix more leaks.

--- expr.c
+++ expr.c
@@ -28,11 +28,14 @@ Expr* expr_neg_new(Expr* neg_expr) {
 void expr_free(Expr* expr) {
     switch (expr->expr_type) {
     case ET_LIST: {
+        if (expr->expr_children == NULL)
+            break;
         Expr* child;
         while ((child = list_next(expr->expr_children, NULL)) != NULL) {
             list_unlink(&child->expr_node);
             expr_free(child);
         }
+        free(expr->expr_children);
         break; }
     case ET_NEG:
         expr_free(expr->expr_neg_expr);
@@ -58,8 +61,10 @@ int expr_eval(Expr* expr) {
     case ET_LIST: {
         int value = 0; // empty expr is false
         Expr* child = NULL;
-        while ((child = list_next(expr->expr_children, child)) != NULL)
-            value |= expr_eval(child);
+        while ((child = list_next(expr->expr_children, child)) != NULL) {
+            int cvalue = expr_eval(child);
+            value = (cvalue < 0) ? -1 : (value | cvalue);
+        }
         return value; }
     case ET_NEG: {
         int value = expr_eval(expr->expr_neg_expr);
@@ -73,12 +78,12 @@ int expr_eval(Expr* expr) {
     }
 }
 
-static void expr_add_vars(List* vars, Expr* expr) {
+static void expr_find_vars(Expr* expr, List* vars) {
     switch (expr->expr_type) {
     case ET_LIST: {
         Expr* child = NULL;
         while ((child = list_next(expr->expr_children, child)) != NULL)
-            expr_add_vars(vars, child);
+            expr_find_vars(child, vars);
         break; }
     case ET_VAR: {
         Var* var = NULL;
@@ -92,7 +97,7 @@ static void expr_add_vars(List* vars, Expr* expr) {
         list_insert(&nvar->v_node, var == NULL ? vars->ls_tail : var->v_node.ln_prev);
         break; }
     case ET_NEG: 
-        expr_add_vars(vars, expr->expr_neg_expr);
+        expr_find_vars(expr->expr_neg_expr, vars);
         break;
     default:
         assert(0);
@@ -102,7 +107,7 @@ static void expr_add_vars(List* vars, Expr* expr) {
 List* expr_vars(Expr* expr, List* vars) {
     if (vars == NULL)
         vars = list_new();
-    expr_add_vars(vars, expr);
+    expr_find_vars(expr, vars);
     return vars;
 }
 
@@ -173,6 +178,7 @@ int expr_eq(Expr* expr1, Expr* expr2) {
 typedef struct MemBuf { char* buf; size_t len; } MemBuf;
 
 static void membuf_addchar(MemBuf* mbuf, char ch) {
+    if (mbuf->len == 0) return;
     *mbuf->buf++ = ch;
     mbuf->len--;
 }
